e41c2c5a72a8134a323665b59fbfba47c3e2450a
dmitriysturov <sturovdmitriyal@gmail.com>
2025-06-09 01:50:04 +0500

    init

--- config/database.php
+++ config/database.php
@@ -82,19 +82,17 @@ return [
             ]) : [],
         ],
 
-        'pgsql' => [
-            'driver' => 'pgsql',
-            'url' => env('DB_URL'),
-            'host' => env('DB_HOST'),
-            'port' => env('DB_PORT'),
-            'database' => env('DB_DATABASE'),
-            'username' => env('DB_USERNAME'),
-            'password' => env('DB_PASSWORD'),
-            'charset' => env('DB_CHARSET', 'utf8'),
-            'prefix' => '',
-            'prefix_indexes' => true,
-            'search_path' => 'public',
-            'sslmode' => 'prefer',
+       'pgsql' => [
+        'driver' => 'pgsql',
+        'host' => '127.0.0.1', 
+        'port' => 5432,
+        'database' => 'digital',
+        'username' => 'digital',
+        'password' => 'digital',
+        'charset' => 'utf8',
+        'prefix' => '',
+        'prefix_indexes' => true,
+        'search_path' => 'public',
         ],
 
         'sqlsrv' => [
