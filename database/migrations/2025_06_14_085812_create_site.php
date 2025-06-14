a52982c41d8532614b9e2f8cd1aa982f2317b2a1
dmitriysturov <sturovdmitriyal@gmail.com>
2025-06-14 14:58:42 +0500

    add model Site

---
+++ database/migrations/2025_06_14_085812_create_site.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('site', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('url');
+            $table->unsignedBigInteger('owner_id');
+            $table->timestamps();
+
+            $table->foreign('owner_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('site');
+    }
+};
